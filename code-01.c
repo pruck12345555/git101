c47eec645c325b0378c8e216d4eaab37c69fc771
Suphawat <suphawat.so@ku.th>
2024-02-09 22:04:18 +0700

    delete text from assets

---
+++ code-01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    char text[] = "Hello Git";
+    printf("%s\n",text);
+    return 0;
+}
---
+++ code-02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello Git in C++" << endl;
+
+
+    return 0;
+}
